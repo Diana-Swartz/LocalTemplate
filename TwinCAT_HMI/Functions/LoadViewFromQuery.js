01dad13921e2dbd24e07462ab2e45bbca8d0e026
Diana-Swartz <141796507+Diana-Swartz@users.noreply.github.com>
2023-08-10 08:32:04 +1200

    Initial commit

---
+++ TwinCAT_HMI/Functions/LoadViewFromQuery.js
@@ -0,0 +1,33 @@
+
+(function (/** @type {globalThis.TcHmi} */ TcHmi) {
+    var Functions;
+    (function (/** @type {globalThis.TcHmi.Functions} */ Functions) {
+        var demo_multi_screen;
+        (function (demo_multi_screen) {
+            function LoadViewFromQuery() {
+
+                var view = GetQueryParameter('view');
+
+                if (!view) return;
+
+                TcHmi.View.load(view + '.view', function (data) {});
+
+                function GetQueryParameter(parameterName) {
+
+                    var href = window.location.href;
+                    var name = parameterName.replace(/[\[\]]/g, '\\$&');
+                    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'), value = regex.exec(href);
+
+                    if (!value) return null;
+                    if (!value[2]) return null;
+
+                    return decodeURIComponent(value[2].replace(/\+/g, ' '));
+
+                }
+
+            }
+            demo_multi_screen.LoadViewFromQuery = LoadViewFromQuery;
+        })(demo_multi_screen = Functions.demo_multi_screen || (Functions.demo_multi_screen = {}));
+        Functions.registerFunctionEx('LoadViewFromQuery', 'TcHmi.Functions.demo_multi_screen', demo_multi_screen.LoadViewFromQuery);
+    })(Functions = TcHmi.Functions || (TcHmi.Functions = {}));
+})(TcHmi);
